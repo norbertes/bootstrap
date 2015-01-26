d3dbb1078fd310ff68cf60e0ffd524b50254e95b
Norbert Sienkiewicz <norbert.sienkiewicz@gmail.com>
2015-01-26 15:30:19 +0100

    fix(repo): version change for compability with tag

--- dist/ui-bootstrap-tpls-0.10.3.js
+++ dist/ui-bootstrap-tpls-0.10.3.js
@@ -2,7 +2,7 @@
  * angular-ui-bootstrap
  * http://angular-ui.github.io/bootstrap/
 
- * Version: 0.10.1 - 2015-01-26
+ * Version: 0.10.3 - 2015-01-26
  * License: MIT
  */
 angular.module("ui.bootstrap", ["ui.bootstrap.tpls", "ui.bootstrap.transition","ui.bootstrap.collapse","ui.bootstrap.accordion","ui.bootstrap.alert","ui.bootstrap.bindHtml","ui.bootstrap.buttons","ui.bootstrap.carousel","ui.bootstrap.position","ui.bootstrap.datepicker","ui.bootstrap.dropdownToggle","ui.bootstrap.modal","ui.bootstrap.pagination","ui.bootstrap.tooltip","ui.bootstrap.popover","ui.bootstrap.progressbar","ui.bootstrap.rating","ui.bootstrap.tabs","ui.bootstrap.timepicker","ui.bootstrap.typeahead"]);
